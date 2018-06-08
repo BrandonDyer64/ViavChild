aca1efdba2584d2cad6071dd04fc97fd57e1cf44
Braxton Fair <me@taxborn.me>
2018-06-07 23:14:55 -0500

    Add express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-// index
+const express = require('express')
+const { client } = require('./discordClient')
+const configSecret = require('../config.secret.json')
+
+const app = express()
+
+app.get('/play', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(3000, () => console.log('Viav listening on port 3000'))
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.channel.send('pong')
+  }
+})
+
+client.on('error', console.error)
+
+client.login(configSecret.token).then(() => {
+  client.user.setPresence({
+    game: {
+      name: 'viav.app',
+      type: 'LISTENING'
+    },
+    status: 'invisible'
+  })
+})
