833571730ec38746a01b9ec5062d28418b7c31d7
Brandon Dyer <brandondyer64@gmail.com>
2018-06-07 21:41:45 -0700

    not working

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const { client } = require('./discordClient')
 const configSecret = require('../config.secret.json')
+const { VoiceChannel } = require('discord.js')
+const bodyParser = require('body-parser')
+var multer = require('multer') // v1.0.5
+var upload = multer() // for parsing multipart/form-data
 
 const app = express()
 
-app.get('/play', (req, res) => {
-  res.send('hello world')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+function voiceChannel(guildId, channelId) {
+  return new VoiceChannel(client.guilds.get(guildId), { id: channelId })
+}
+
+app.post('/play', (req, res) => {
+  console.log(req.body)
+  voiceChannel(req.body.guild.id, req.body.voiceChannel.id).join()
+  console.log('ouch')
+  res.json({ success: true, echo: req.body })
 })
 
 app.listen(3000, () => console.log('Viav listening on port 3000'))
@@ -24,6 +38,6 @@ client.login(configSecret.token).then(() => {
       name: 'viav.app',
       type: 'LISTENING'
     },
-    status: 'invisible'
+    status: 'online' // invisible
   })
 })
